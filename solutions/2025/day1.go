6d429476a8edeec8245f20b802c760cde454fc8d
Chase Rensberger <chasehrensberger@gmail.com>
2025-12-01 22:52:31 -0500

    2025 day 1 done

--- solutions/2025/day1.go
+++ solutions/2025/day1.go
@@ -2,11 +2,20 @@ package y2025
 
 import (
 	"log"
+	"math"
 	"strconv"
 	"strings"
 )
 
 type Day1 struct{}
+
+func abs(num int) int {
+	return int(math.Abs(float64(num)))
+}
+
+func mod(a, b int) int {
+	return ((a % b) + b) % b
+}
 
 func (*Day1) SolvePartOne(input string) string {
 	rotations := strings.Split(strings.TrimSpace(input), "\n")
@@ -20,11 +29,10 @@ func (*Day1) SolvePartOne(input string) string {
 		}
 		if dir == 'L' {
 			dial -= dis
-			dial = ((dial % 100) + 100) % 100
 		} else {
 			dial += dis
-			dial %= 100
 		}
+		dial = mod(dial, 100)
 		if dial == 0 {
 			ans++
 		}
@@ -33,33 +41,39 @@ func (*Day1) SolvePartOne(input string) string {
 }
 
 func (*Day1) SolvePartTwo(input string) string {
+	var newPos int
+	var lasPos int
 	rotations := strings.Split(strings.TrimSpace(input), "\n")
 	dial := 50
 	ans := 0
 	for _, r := range rotations {
-		// fmt.Printf("dial: %v\n", dial)
+		newPos = dial
 		dir := r[0]
 		dis, err := strconv.Atoi(r[1:])
 		if err != nil {
 			log.Fatal(err)
 		}
 		if dir == 'L' {
-			dial -= dis
-			if dial < 0 {
-				ans += (dial + 99) / 100
-			}
-			dial = ((dial % 100) + 100) % 100
+			newPos -= dis
 		} else {
-			dial += dis
-			if dial > 99 {
-				ans += (dial / 100)
-			}
-			dial %= 100
+			newPos += dis
 		}
+		lasPos = dial
+		dial = mod(newPos, 100)
 		if dial == 0 {
 			ans++
 		}
-		// fmt.Printf("ans: %v\n", ans)
+		if newPos > 99 {
+			ans += (newPos / 100)
+			if dial == 0 {
+				ans--
+			}
+		} else if newPos < 0 {
+			ans += (abs(newPos) + 99) / 100
+			if lasPos == 0 {
+				ans--
+			}
+		}
 	}
 	return strconv.Itoa(ans)
 }
